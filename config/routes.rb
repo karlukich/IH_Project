27e2e40ad7d881c327d8de4ed7dbc583b0b76787
Carlos Castillo Ibañez <carloscastilloibanyez@gmail.com>
2017-02-22 12:37:36 +0100

    Login, logout, signin, recover pass OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :passwords => "passwords" }
   get '/', to: 'site#index'
-  get '/users/:id', to: 'users#show'
+  get '/users/:id', to: 'users#show', as: :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
